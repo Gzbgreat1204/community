2423ef419bac7678c62a748d0a2717cb1acd12d8
Gzbgreat1204 <1501590573@qq.com>
2020-07-14 21:23:42 +0800

    增加MarkDown语法编辑

--- src/main/java/life/majiang/community/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/community/controller/PublishController.java
@@ -31,7 +31,6 @@ public class PublishController {
         model.addAttribute("description", question.getDescription());
         model.addAttribute("tag", question.getTag());
         model.addAttribute("id", question.getId());
-
         model.addAttribute("tags", TagCache.get());
         return "publish";
     }
